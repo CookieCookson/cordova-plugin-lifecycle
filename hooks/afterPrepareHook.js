171eaf56a63980dd27d56c0d3a0dc7ba5ef0604a
Christian Cook <christian@elixel.co.uk>
2015-12-10 17:15:05 +0000

    pass through variant app id to xcode plist

--- hooks/afterPrepareHook.js
+++ hooks/afterPrepareHook.js
@@ -1,6 +1,9 @@
 var configParser = require('./lib/configXmlParser.js');
 var fs = require('fs-extra');
 var gm = require('gm');
+var path = require('path');
+var plist    = require('plist');
+var parseXML = require('xml2js').parseString;
 
 module.exports = function(ctx) {
     run(ctx);
@@ -62,4 +65,39 @@ function generateAndroidIcons(androidIcons, variant, iconOverlay) {
             }
         });
     });
+}
+
+function updateProductBundleIdentifier(cordovaContext) {
+    var cliCommand = process.argv.join();
+    var PRODUCT_BUNDLE_IDENTIFIER = configParser.getApplicationId(cordovaContext);
+    
+    // Default to alpha variant
+    var VARIANT_TYPE = 'alpha';
+    // Get variant type from command line
+    if (cliCommand.indexOf('--alpha') > -1) {
+        VARIANT_TYPE = 'alpha';
+    } else if (cliCommand.indexOf('--beta') > -1) {
+        VARIANT_TYPE = 'beta';
+    } else if (cliCommand.indexOf('--store') > -1) {
+        VARIANT_TYPE = '';
+    }
+    
+    // Append variant type to product bundle identifier if set
+    if (VARIANT_TYPE !== '') {
+        PRODUCT_BUNDLE_IDENTIFIER = PRODUCT_BUNDLE_IDENTIFIER + '.' + VARIANT_TYPE;
+        // Change CFBundleIdentifier to new product bundle identifier
+        var projectRoot = cordovaContext.opts.projectRoot;
+        var configFile = path.join(projectRoot, 'config.xml');
+        parseXML(fs.readFileSync(configFile, 'utf8'), function(err, result) {
+        if (err) { throw err; }
+        var config = result.widget;
+        var projectName = config.name[0].trim();
+        var plistFile = path.join(projectRoot, '/platforms/ios', projectName, projectName+'-Info.plist');
+        var plistObj = plist.parse(fs.readFileSync(plistFile, 'utf8'));
+        Object.assign(plistObj, {
+            CFBundleIdentifier: PRODUCT_BUNDLE_IDENTIFIER
+        });
+        fs.writeFileSync(plistFile, plist.build(plistObj));
+        });
+    }
 }
--- hooks/lib/configXmlParser.js
+++ hooks/lib/configXmlParser.js
@@ -4,7 +4,11 @@
 
     module.exports = {
         getIcons: getIcons,
+<<<<<<< HEAD
         getIconOverlay: getIconOverlay
+=======
+        getApplicationId: getApplicationId
+>>>>>>> pass through variant app id to xcode plist
     };
     
     function getIconOverlay(cordovaContext, variant) {
@@ -50,6 +54,15 @@
             });      
         });
         return iconArray;
+    }
+    
+    function getApplicationId(cordovaContext) {
+        var configXml = new ConfigXmlHelper(cordovaContext).read();
+        if (configXml == null) {
+            console.warn('config.xml not found! Please, check that it exist\'s in your project\'s root directory.');
+            return null;
+        }
+        return configXml['widget']['$']['id'];
     }
 
 })();
